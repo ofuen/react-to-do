423063923c471456c124347cc8a2dc94b7b0bc8b
oscar <oscar@o2webdev.com>
2019-08-03 14:45:24 -0400

    Updates

---
+++ precache-manifest.f053df71f34e0f4f479084f714be1db4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de75d7bfdd984b80341b",
+    "url": "/react-to-do/static/js/runtime~main.de75d7bf.js"
+  },
+  {
+    "revision": "57f949956c352570ff9c",
+    "url": "/react-to-do/static/js/main.57f94995.chunk.js"
+  },
+  {
+    "revision": "e5f1bd10652fc6b90e34",
+    "url": "/react-to-do/static/js/1.e5f1bd10.chunk.js"
+  },
+  {
+    "revision": "57f949956c352570ff9c",
+    "url": "/react-to-do/static/css/main.5f04aac3.chunk.css"
+  },
+  {
+    "revision": "a86fd82f7ec879354a13816c7e34f595",
+    "url": "/react-to-do/index.html"
+  }
+];
