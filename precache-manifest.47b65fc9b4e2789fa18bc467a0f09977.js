acda84893b3f0617e99e75a6980f0ec33c5772b4
oscar <oscar@o2webdev.com>
2019-08-08 20:46:51 -0400

    Updates

---
+++ precache-manifest.47b65fc9b4e2789fa18bc467a0f09977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded820315f02354729ac8fbd8cbf1ee2",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "fae66d711858a2ec205a",
+    "url": "/react-to-do/static/css/main.efe9169c.chunk.css"
+  },
+  {
+    "revision": "c4a551e6af96a573775b",
+    "url": "/react-to-do/static/js/2.8da596d3.chunk.js"
+  },
+  {
+    "revision": "fae66d711858a2ec205a",
+    "url": "/react-to-do/static/js/main.db4c3f2d.chunk.js"
+  },
+  {
+    "revision": "26b717aa96a48dc90608",
+    "url": "/react-to-do/static/js/runtime~main.8def430e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-to-do/precache-manifest.f053df71f34e0f4f479084f714be1db4.js"
+  "/react-to-do/precache-manifest.47b65fc9b4e2789fa18bc467a0f09977.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-to-do/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-to-do/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
